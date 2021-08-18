1c401ae759413b7ac19bce5477281b7f92a3e5f1
LetsBeLikeSophie <solip.sophie@gmail.com>
2021-08-18 14:53:06 +0900

    deleting

--- 01_CoreJava/Ex_OracleTest.java
+++
@@ -1,9 +0,0 @@
-
-public class Ex_OracleTest {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
--- 01_CoreJava/common/FilePath.java
+++
@@ -1,9 +0,0 @@
-package a.b.c.common;
-
-public abstract class FilePath {
-
-	public static final String FILE_PATH = "C:/00.KOSMO93_CSI/10.JExam/el_java_work/src/a/b/c/ch7";
-	public static final String FILE_PATH_08 = "C:/00.KOSMO93_CSI/10.JExam/el_java_work/src/a/b/c/ch8";
-
-	}
-
